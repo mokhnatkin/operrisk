fdb1216667cb16ef205867a9cccbef919aade7ff
Мохнаткин Алексей <a.mokhnatkin@a-i.kz>
2018-10-18 16:17:19 +0600

    base templated implemented; population script revised

--- operrisk_project/populate-operrisk.py
+++ operrisk_project/populate-operrisk.py
@@ -12,15 +12,6 @@ from operrisk.models import Category, Incident
 def populate():
     #create list of categories
     cats = [
-
-            """    {"name":"Внутреннее мошенничество","URL_name":"internal-fraud"},
-                {"name":"Внешнее мошенничество","URL_name":"external-fraud"},
-                {"name":"Клиенты, продукты и деловая практика","URL_name":"clients-products-practice"},
-                {"name":"Сбой ведения бизнеса и работы систем","URL_name":"system-failure"},
-                {"name":"Исполнение, доставка и управление процессом","URL_name":"process-management"},
-                {"name":"Производственные отношения и безопасность труда","URL_name":"labor-safety"},
-                {"name":"Стихийные бедствия и безопасность","URL_name":"natural-disasters"} """
-
                 {"name":"Внутреннее мошенничество"},
                 {"name":"Внешнее мошенничество"},
                 {"name":"Клиенты, продукты и деловая практика"},
@@ -30,10 +21,10 @@ def populate():
                 {"name":"Стихийные бедствия и безопасность"}
             ]
     for cat in cats:
-        c = add_cat(cat["name"],cat["URL_name"])
+        c = add_cat(cat["name"])
 
-def add_cat(name, URL_name):
-    c = Category.objects.get_or_create(name=name,URL_name=URL_name)[0]
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
     c.save()
     return c
 
